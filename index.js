dd4572638cbb26bd09b3b7f69cda7d096fc38373
Helena Šegedin <helena.segedin@vikk.ee>
2023-09-24 20:58:13 +0300

    As a user, I want to be able to log in and out of the SaaS Garden Plant Tracker so that I can access my account and protect my privacy.[#186105985]

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000
+const {v4: uuidv4} = require('uuid')
 
 // Add Swagger UI
 const swaggerUi = require('swagger-ui-express');
@@ -15,8 +16,24 @@ app.use(express.static('public'))
 app.use(express.json())
 
 const users = [
-    {id: 1, email: 'admin', password: 'admin'}
+    {id: 1, email: 'admin', password: '$2b$10$0EfA6fMFRDVQWzU0WR1dmelPA7.qSp7ZYJAgneGsy2ikQltX2Duey'}
 ]
+
+let sessions = [
+    // {id: '123', userId: 1}
+]
+
+function tryToParseJson(jsonString) {
+    try {
+        const o = JSON.parse(jsonString);
+        if (o && typeof o === "object") {
+            return o;
+        }
+    } catch (e) {
+    }
+    return false;
+}
+
 app.post('/users', async (req, res) => {
 
     // Validate email and password
@@ -35,7 +52,11 @@ app.post('/users', async (req, res) => {
         }
         console.log('Email verified')
     } catch (error) {
-        return res.status(400).send('Email is invalid: ' + error.code)
+        const errorObject = tryToParseJson(error)
+        if (errorObject && errorObject.info) {
+            return res.status(400).send('Email is invalid: ' + errorObject.info)
+        }
+        return res.status(400).send('Email is invalid: ' + error)
     }
 
     // Hash password
@@ -46,14 +67,86 @@ app.post('/users', async (req, res) => {
         console.error(error);
     }
 
-
     // Find max id
     const maxId = users.reduce((max, user) => user.id > max ? user.id : max, users[0].id)
 
     // Save user to database
+    console.log(hashedPassword)
     users.push({id: maxId + 1, email: req.body.email, password: hashedPassword})
 
     res.status(201).end()
+
+})
+
+// POST /sessions
+app.post('/sessions', async (req, res) => {
+
+    // Validate email and password
+    if (!req.body.email || !req.body.password) return res.status(400).send('Email and password are required')
+
+    // Find user in database
+    const user = users.find(user => user.email === req.body.email)
+    if (!user) return res.status(404).send('User not found')
+
+    // Compare password
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+
+            // Create session
+            const session = {id: uuidv4(), userId: user.id}
+
+            // Add session to sessions array
+            sessions.push(session)
+
+            // Send session to client
+            res.status(201).send(session)
+        } else {
+            // Passwords don't match
+            res.status(401).send('Invalid password')
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal server error')
+    }
+
+})
+
+function authorizeRequest(req, res, next) {
+    // Check that there is an Authorization header
+    if (!req.headers.authorization) return res.status(401).send('Authorization header is missing')
+
+    // Check that the Authorization header is in the correct format
+    const authorizationHeader = req.headers.authorization.split(' ')
+    if (authorizationHeader.length !== 2 || authorizationHeader[0] !== 'Bearer') return res.status(401).send('Invalid Authorization header')
+
+    // Get the session id from the Authorization header
+    const sessionId = authorizationHeader[1]
+
+    // Find the session in the sessions array
+    const session = sessions.find(session => session.id === sessionId)
+    if (!session) return res.status(401).send('Invalid session id')
+
+    // Check that the user exists
+    const user = users.find(user => user.id === session.userId)
+    if (!user) return res.status(401).send('Invalid user id')
+
+    // Add user to request object
+    req.user = user
+
+    // Add session to request object
+    req.session = session
+
+    // Call next middleware
+    next()
+
+}
+
+app.delete('/sessions', authorizeRequest, (req, res) => {
+
+    // Remove session from sessions array
+    sessions = sessions.filter(session => session.id !== req.session.id)
+
+    res.status(204).end()
 
 })
 
@@ -66,7 +159,7 @@ function verifyEmail(email) {
         verifier.verify(email, (err, info) => {
             console.log(err, info);
             if (err) {
-                reject(JSON.stringify(err));
+                reject(JSON.stringify([err, info]));
             } else {
                 resolve(info);
             }
